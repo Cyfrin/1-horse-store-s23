87e2e0a736569022a22b1ac6efe525f8cd0d17d6
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2023-10-30 21:49:59 -0400

    needed to do the runtime

--- test/Base_Test.t.sol
+++ test/Base_Test.t.sol
@@ -6,15 +6,24 @@ import {HorseStore} from "../src/HorseStore.sol";
 import {HorseStoreYul} from "../src/HorseStoreYul.sol";
 import {Test, console2} from "forge-std/Test.sol";
 
+contract Dummy {}
+
 abstract contract Base_Test is Test {
     string public constant horseStoreLocation = "HorseStore";
     HorseStore horseStoreHuff;
-    // HorseStoreYul horseStoreYul;
+    HorseStoreYul horseStoreYul;
     HorseStore horseStoreSol;
 
     function setUp() public virtual {
-        horseStoreHuff = HorseStore(HuffDeployer.config().deploy(horseStoreLocation));
-        // horseStoreYul = new HorseStoreYul();
+        // horseStoreHuff = HorseStore(HuffDeployer.config().deploy(horseStoreLocation));
+        horseStoreHuff = HorseStore(address(new Dummy()));
+
+        vm.etch(
+            address(horseStoreHuff),
+            hex"5f3560e01c8063cdfead2e1461001b578063e026c01714610022575b6004355f55005b5f545f5260205ff3"
+        );
+
+        horseStoreYul = new HorseStoreYul();
 
         // in halmos, this is deployed to `2863267842` aka 0xaaaa0002
         horseStoreSol = new HorseStore();
